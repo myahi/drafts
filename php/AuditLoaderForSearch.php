1e3c3c5ca6ff200dccbc23493f1da5cf4a60e0b3
myahi <metnique@gmail.com>
2025-12-25 12:02:40 +0100

    Update AuditLoaderForSearch.php

--- php/AuditLoaderForSearch.php
+++ php/AuditLoaderForSearch.php
@@ -1,9 +1,9 @@
 <?php
 
+include_once 'AuditDAO.php';
+include_once 'AuditKeywordDAO.php';
 include_once 'connexion.php';
-
-try{
-	// Default timezone
+// Default timezone
 	if( ! ini_get('date.timezone') ) {
 		date_default_timezone_set('Europe/Paris');
 	}	
@@ -21,6 +21,8 @@ try{
 			'TIMESTAMP' => '',
 			'INSERT_TIMESTAMP' => '',
 			'DETAILS' => '' ,
+			'FLOW' => '',
+			'DISPLAYED_FLOW_TAGS' => ''
 	);
 	
 	$pagination = 0;
@@ -35,11 +37,14 @@ try{
 		$criteres['TIMESTAMP'] = date("Y-m-d H:i", mktime(0, 0, 0, date("m"), date("d"), date("Y")));
 		$criteres['TIMESTAMP2'] = date("Y-m-d H:i", mktime(0, 0, 0, date("m"), date("d")+1, date("Y")));
 		$criteres['audit_metadata'] = null;
+		$criteres['FLOWS'] = '';
 
 		$_SESSION['audit_metadata'] = null;
 		$_SESSION['recherche_code'] = null;
 		$_SESSION['recherche_codifier'] = null;
 		$_SESSION['recherche_project'] = null;
+		$_SESSION['recherche_flows'] = null;
+		$_SESSION['recherche_displayed_flow_tags'] = null;
 		$_SESSION['recherche_status'] = null;
 		$_SESSION['recherche_data'] = null;
 		$_SESSION['recherche_timestamp'] = null;
@@ -48,7 +53,6 @@ try{
 		$_SESSION['recherche_details'] = null;
 		$_SESSION['recherche_intervalle'] = null;
 		$_SESSION['useArchiveAudit'] = null;
-		$_SESSION['TOTAOL_AUDIT_NB_LINE'] = 0;
 
 		$_POST['code'] = null;
 		$_POST['codifier'] = null;
@@ -64,6 +68,7 @@ try{
 		$_POST['intervalle'] = null;
 		$_POST['useArchiveAudit'] = null;
 		$_GET['pagination'] = null;
+		$_POST['FLOWS'] = null;
 	} else {
 		// Valorisation des critères de recherche
 		// RECHERCHE 1/2
@@ -80,6 +85,14 @@ try{
 		if (isset($_POST['PROJECT']) || !empty( $_POST['PROJECT'])){
 			$criteres['PROJECT'] = $_POST['PROJECT'];
 			$_SESSION['recherche_project'] = $_POST['PROJECT'];
+		}
+		if (isset($_POST['FLOWS']) || !empty( $_POST['FLOWS'])){
+			$criteres['FLOWS'] = $_POST['FLOWS'];
+			$_SESSION['recherche_flows'] = $_POST['FLOWS'];
+		}
+		if (isset($_POST['DISPLAYED_FLOW_TAGS']) || !empty( $_POST['DISPLAYED_FLOW_TAGS'])){
+			$criteres['DISPLAYED_FLOW_TAGS'] = $_POST['DISPLAYED_FLOW_TAGS'];
+			$_SESSION['recherche_displayed_flow_tags'] = $_POST['DISPLAYED_FLOW_TAGS'];
 		}
 		
 		if (isset($_POST['STATUS']) || !empty ($_POST['STATUS'])){
@@ -123,7 +136,7 @@ try{
 											
 			$_SESSION['audit_metadata'] = $criteres['audit_metadata'];
 		} else {
-			// Aucun autre critère de recherche
+			// Aucun autre crit�re de recherche
 			$_SESSION['audit_metadata'] = null;
 			$criteres['audit_metadata'] = null;
 		}
@@ -184,8 +197,119 @@ try{
 		$_SESSION['recherche_intervalle'] = $_POST['intervalle'];
 	}
 	
-	header('Location: auditView.php');
-} catch(Exception $e) {
-	header('Location: auditView.php?error='.$e->getMessage()); 
-}
+// Turn off all error reporting
+error_reporting (0);
+try {
+	// -------------------------- VERIFICATION DE LA SESSION Et DES DROITS -------------------------------------
+	$_GET['screenId'] = $GLOBALS['SCREEN_AUDIT_PREFIX'];
+	include 'verification.php';
+	
+	// ---------------------------------- RECHERCHE 1/2 --------------------------------------
+	$criteres = array (
+			'CODE' => '',
+			'CODIFIER' => '',
+			'PROJECT' => '',
+			'STATUS' => '',
+			'DATA' => '',
+			'TIMESTAMP' => '',
+			'INSERT_TIMESTAMP' => '',
+			'DETAILS' => '',
+			'FLOWS' => '',
+			'DISPLAYED_FLOW_TAGS' => ''
+	);
+	
+	$pagination = 0;
+	// RECHERCHE 1/2
+	if ((isset($_SESSION['recherche_code'])) || (!empty($_SESSION['recherche_code']))) {
+			$criteres ['CODE'] = $_SESSION ['recherche_code'];
+	}
+	
+	if ((isset($_SESSION['recherche_codifier'])) || (!empty($_SESSION['recherche_codifier']))) {
+			$criteres ['CODIFIER'] = $_SESSION ['recherche_codifier'];
+	}
+	$useAuditArchive = null;
+	if ((isset($_SESSION['recherche_useArchiveAudit'])) || (!empty($_SESSION['recherche_useArchiveAudit']))) {
+		$criteres ['useArchiveAudit'] = $_SESSION ['recherche_useArchiveAudit'];
+		$useAuditArchive = true;	
+	}
+	if ((isset($_SESSION['recherche_project'])) || (!empty($_SESSION['recherche_project']))) {
+			$criteres ['PROJECT'] = $_SESSION ['recherche_project'];
+	}
+	if ((isset($_SESSION['recherche_flows'])) || (!empty($_SESSION['recherche_flows']))) {
+			$criteres ['FLOWS'] = $_SESSION ['recherche_flows'];
+	}
+	if ((isset($_SESSION['recherche_displayed_flow_tags'])) || (!empty($_SESSION['recherche_displayed_flow_tags']))) {
+			$criteres ['DISPLAYED_FLOW_TAGS'] = $_SESSION ['recherche_displayed_flow_tags'];
+	}
+
+	if ((isset($_SESSION['recherche_status'])) || (!empty($_SESSION['recherche_status']))) {
+			$criteres ['STATUS'] = $_SESSION ['recherche_status'];
+	}
+	
+	if ((isset($_SESSION['recherche_data'])) || (!empty($_SESSION['recherche_data']))) {
+			$criteres ['DATA'] = $_SESSION ['recherche_data'];
+	}
+	
+	if ((isset ( $_SESSION ['audit_metadata'] )) || (! empty ( $_SESSION ['audit_metadata'] ))){
+		$criteres ['audit_metadata'] = $_SESSION ['audit_metadata'];
+	}
+	
+	if ((isset($_SESSION['recherche_timestamp'])) || (!empty($_SESSION['recherche_timestamp']))) {
+		$criteres ['TIMESTAMP'] = $_SESSION ['recherche_timestamp'];
+	}
+	if ((! isset ( $criteres ['TIMESTAMP'] )) || (empty ( $criteres ['TIMESTAMP'] ))) {
+		$criteres ['TIMESTAMP'] = date("d-m-Y H:i", mktime(0, 0, 0, date("m"), date("d"), date("Y")));
+	}
+
+	if ((isset($_SESSION['recherche_timestamp2'])) || (!empty($_SESSION['recherche_timestamp2']))) {
+			$criteres ['TIMESTAMP2'] = $_SESSION ['recherche_timestamp2'];
+	}
+
+	if ((! isset ( $criteres ['TIMESTAMP2'] )) || (empty ( $criteres ['TIMESTAMP2'] ))) { 
+			$criteres ['TIMESTAMP2'] = date("d-m-Y H:i", mktime(0, 0, 0, date("m"), date("d")+1, date("Y")));
+	}
+		
+	if ((isset ( $_SESSION ['recherche_details'] )) || (! empty ( $_SESSION ['recherche_details'] ))) {
+		$criteres ['DETAILS'] = $_SESSION ['recherche_details'];
+	}
+
+	$orderColumn = "";
+	if (! empty ( $_SESSION ['recherche_store'] ) && strpos ( "-CODE-CODIFIER-PROJECT-DATA-TIMESTAMP-STATUT", $_SESSION ['recherche_store'] ) == true) {
+			$orderColumn = $_SESSION ['recherche_store'];
+	}
+	
+	if ((isset ( $_SESSION ['recherche_SENS_AFF'] )) || (! empty ( $_SESSION ['recherche_SENS_AFF'] ))) {
+		$orderDirection = $_SESSION ['recherche_SENS_AFF'];
+	} else {
+		$orderDirection = "desc";
+	}
+	
+	$pagination = isset($_GET['offset']) ? (int)$_GET['offset'] : 0;
+	
+	//$intervalle = 50;
+	$intervalle = isset($_GET['limit']) ? (int)$_GET['limit'] : 50;
+	
+	// Ensemble des Status 
+	// RECHERCHE 2/2
+	
+	$req_dump = print_r($auditInformations, TRUE);
+	$fp = fopen('/serveur_apps/tmp/auditInformations.log', 'w');
+	fwrite($fp, $req_dump);
+	fclose($fp);
+
+	if ((isset ( $_SESSION ['descriptionList'] )) || (! empty ( $_SESSION ['descriptionList'] ))) {
+		$data = $_SESSION ['descriptionList'];
+	} else {
+		$data = $auditKeywordDAO -> getAuditDescriptionsList();
+		$_SESSION ['descriptionList'] = $data;
+	}
+	
+	if ((isset ( $_SESSION ['codifiersList'] )) || (! empty ($_SESSION ['codifiersList'] ))) {
+		$codifiers = $_SESSION ['codifiersList'];
+	} else {
+		$codifiers = $auditKeywordDAO -> getAuditCodifiersList();
+		$_SESSION ['codifiersList'] = $codifiers;
+	}
+
+	} catch ( Exception $e ) { }
 ?>
