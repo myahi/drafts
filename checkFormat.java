c478029d89eb34086e65db10e26d18fbb51ec8d3
myahi <metnique@gmail.com>
2025-12-30 22:02:29 +0100

    Create checkFormat.java

---
+++ checkFormat.java
@@ -0,0 +1,68 @@
+private boolean checkFormat(RGVmessage rgvMessage, Map<String, Transcos> transcosByCategory) {
+
+    int typeRemunerationLen = Optional.ofNullable(rgvMessage)
+            .map(RGVmessage::getRGVchamps)
+            .map(RGVchamps::getTypeRemuneration)
+            .map(String::trim)
+            .map(String::length)
+            .orElse(0);
+
+    if (typeRemunerationLen == 0 &&
+        findTransco(
+                transcosByCategory,
+                "rgv.mt065.typeRemuneration.filter",
+                Map.of("srcApp", "Calypso", "srcVl", "YES")
+        ) != null) {
+        return true;
+    }
+
+    int garanteeLen = Optional.ofNullable(rgvMessage)
+            .map(RGVmessage::getRGVchamps)
+            .map(RGVchamps::getEtablissementEmetteur)
+            .map(EtablissementEmetteur::getGarantee)
+            .map(String::trim)
+            .map(String::length)
+            .orElse(0);
+
+    if (garanteeLen == 0 || garanteeLen > 1) {
+        return true;
+    }
+
+    int programIdLen = Optional.ofNullable(rgvMessage)
+            .map(RGVmessage::getRGVchamps)
+            .map(RGVchamps::getEtablissementEmetteur)
+            .map(EtablissementEmetteur::getProgramId)
+            .map(String::trim)
+            .map(String::length)
+            .orElse(0);
+
+    if ((programIdLen == 0 &&
+         findTransco(
+                 transcosByCategory,
+                 "rgv.mt065.programId.filter",
+                 Map.of("srcApp", "Calypso", "srcVl", "YES")
+         ) != null)
+        || programIdLen > 6) {
+        return true;
+    }
+
+    int programOriginLen = Optional.ofNullable(rgvMessage)
+            .map(RGVmessage::getRGVchamps)
+            .map(RGVchamps::getEtablissementEmetteur)
+            .map(EtablissementEmetteur::getProgramOrigin)
+            .map(String::trim)
+            .map(String::length)
+            .orElse(0);
+
+    if ((programOriginLen == 0 &&
+         findTransco(
+                 transcosByCategory,
+                 "rgv.mt065.programOrigin.filter",
+                 Map.of("srcApp", "Calypso", "srcVl", "YES")
+         ) != null)
+        || programOriginLen > 3) {
+        return true;
+    }
+
+    return false;
+}
