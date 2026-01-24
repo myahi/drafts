96cafc5ea99c22080525db94111720195e716bc8
myahi <metnique@gmail.com>
2026-01-24 21:55:43 +0100

    Create CsvParseException.java

---
+++ csv/CsvParseException.java
@@ -0,0 +1,8 @@
+package com.mycompany.eai.camel.core.csv;
+
+public class CsvParseException extends RuntimeException {
+
+    public CsvParseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
