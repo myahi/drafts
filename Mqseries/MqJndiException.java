525073a7713e3d85c4743f2d821aa309ba421ac0
myahi <metnique@gmail.com>
2026-01-25 19:57:09 +0100

    Create MqJndiException.java

---
+++ Mqseries/MqJndiException.java
@@ -0,0 +1,7 @@
+package com.mycompany.app.mq;
+
+public class MqJndiException extends RuntimeException {
+    public MqJndiException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
