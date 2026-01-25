251162ddac0a065d257c5b65554430b154d08772
myahi <metnique@gmail.com>
2026-01-25 20:08:49 +0100

    Update MqJndiConfig.java

--- Mqseries/MqJndiConfig.java
+++ Mqseries/MqJndiConfig.java
@@ -13,36 +13,48 @@ import java.util.Hashtable;
 @Configuration
 public class MqJndiConfig {
 
-    @Bean(destroyMethod = "close")
-    public Context mqJndiContext(
-            @Value("${mq.jndi.bindingsDir}") Path bindingsDir
+    @Bean(name = "mqJndiContextA", destroyMethod = "close")
+    public Context mqJndiContextA(@Value("${mq.a.jndi.bindingsDir}") Path bindingsDir) {
+        return createContext(bindingsDir, "A");
+    }
+
+    @Bean(name = "mqJndiContextB", destroyMethod = "close")
+    public Context mqJndiContextB(@Value("${mq.b.jndi.bindingsDir}") Path bindingsDir) {
+        return createContext(bindingsDir, "B");
+    }
+
+    @Bean(name = "mqConnectionFactoryA")
+    public ConnectionFactory mqConnectionFactoryA(
+            @Value("${mq.a.jndi.connectionFactory}") String cfJndiName,
+            @javax.annotation.Resource(name = "mqJndiContextA") Context ctx
     ) {
+        return lookupCf(ctx, cfJndiName, "A");
+    }
+
+    @Bean(name = "mqConnectionFactoryB")
+    public ConnectionFactory mqConnectionFactoryB(
+            @Value("${mq.b.jndi.connectionFactory}") String cfJndiName,
+            @javax.annotation.Resource(name = "mqJndiContextB") Context ctx
+    ) {
+        return lookupCf(ctx, cfJndiName, "B");
+    }
+
+    private Context createContext(Path bindingsDir, String label) {
         try {
             Hashtable<String, String> env = new Hashtable<>();
-            env.put(Context.INITIAL_CONTEXT_FACTORY,
-                    "com.sun.jndi.fscontext.RefFSContextFactory");
-            env.put(Context.PROVIDER_URL,
-                    bindingsDir.toUri().toString()); // ex: file:/opt/mq/jndi/
-
+            env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.fscontext.RefFSContextFactory");
+            env.put(Context.PROVIDER_URL, bindingsDir.toUri().toString());
             return new InitialContext(env);
         } catch (Exception e) {
-            throw new MqJndiException(
-                "Unable to initialize JNDI context for bindingsDir=" + bindingsDir, e
-            );
+            throw new MqJndiException("Unable to init JNDI context " + label + " for " + bindingsDir, e);
         }
     }
 
-    @Bean
-    public ConnectionFactory mqConnectionFactory(
-            Context mqJndiContext,
-            @Value("${mq.jndi.connectionFactory}") String cfJndiName
-    ) {
+    private ConnectionFactory lookupCf(Context ctx, String name, String label) {
         try {
-            return (ConnectionFactory) mqJndiContext.lookup(cfJndiName);
+            return (ConnectionFactory) ctx.lookup(name);
         } catch (Exception e) {
-            throw new MqJndiException(
-                "Unable to lookup ConnectionFactory: " + cfJndiName, e
-            );
+            throw new MqJndiException("Unable to lookup ConnectionFactory " + label + ": " + name, e);
         }
     }
 }
