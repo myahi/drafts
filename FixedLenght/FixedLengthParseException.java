98f022f29230d1b906d9a6b0db23f8f4a169037b
myahi <metnique@gmail.com>
2026-01-25 18:26:56 +0100

    Create FixedLengthParseException.java

---
+++ FixedLenght/FixedLengthParseException.java
@@ -0,0 +1,7 @@
+package com.mycompany.eai.camel.core.fixed;
+
+public class FixedLengthParseException extends RuntimeException {
+    public FixedLengthParseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
