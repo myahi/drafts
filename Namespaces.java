343c17cbb76a836976e0533b44947995968774e0
myahi <metnique@gmail.com>
2026-01-31 16:53:13 +0100

    Delete Namespaces.java

--- Namespaces.java
+++
@@ -1,7 +0,0 @@
-package com.mycompany.model;
-
-public final class Namespaces {
-    private Namespaces() {}
-    public static final String AUDITINFO =
-        "http://www.tibco.com/schemas/EAIFramework/framework/resources/XSD/auditInfo.xsd";
-}
