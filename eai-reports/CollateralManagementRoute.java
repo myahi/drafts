0fa4826ee212853f1ab236b56bf10ffceac84072
myahi <metnique@gmail.com>
2026-02-03 09:53:07 +0100

    Create CollateralManagementRoute.java

---
+++ eai-reports/CollateralManagementRoute.java
@@ -0,0 +1,127 @@
+package fr.labanquepostale.report.collateral.routes;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import fr.labanquepostale.marches.eai.core.dollaru.DollarUUtil;
+import fr.labanquepostale.marches.eai.core.helper.AuditHelper;
+import fr.labanquepostale.marches.eai.core.model.audit.Codifier;
+import fr.labanquepostale.marches.eai.core.model.audit.Status;
+
+@Component
+public class CollateralManagementRoute extends RouteBuilder {
+
+    @Autowired
+    private AuditHelper auditHelper;
+
+    @Override
+    public void configure() {
+
+        // Catch-all
+        onException(Exception.class)
+            .handled(true)
+            .process(this::handleException);
+
+        fromF("file:{{eai.report.collateral.input.dir}}"
+            + "?include={{eai.report.collateral.input.file.pattern}}"
+            + "&delay={{eai.report.collateral.input.delay.ms}}"
+            + "&readLock=changed")
+        .routeId("eai-camel-report-collateral-management-route")
+
+        // Cache paths
+        .setProperty("inputFilePath", header("CamelFilePath"))
+        .setProperty("inputFileName", header("CamelFileName"))
+        .setProperty("outputDir", simple("{{eai.report.collateral.output.dir}}"))
+        .setProperty("dollarURessourceName", simple("{{eai.report.collateral.dollaru.ressource}}"))
+
+        // Audit réception
+        .process(exchange -> {
+            auditHelper.audit(exchange)
+                .ref(
+                    Codifier.FILE_NAME.getCodifier(),
+                    exchange.getProperty("inputFileName", String.class)
+                )
+                .desc("Fichier collateral management détecté")
+                .status(Status.Info.getStatus())
+                .data("")
+                .meta("PROCESS_NAME", this.getClass().getName())
+                .send();
+        })
+
+        // Traitement métier
+        .process(this::removeLastLineAndCopy)
+
+        // DollarU OK
+        .process(exchange ->
+            exchange.getMessage().setBody(
+                DollarUUtil.buildOKDollarURequest(
+                    exchange.getProperty("dollarURessourceName", String.class)
+                )
+            )
+        )
+        .to("direct:dollarUCommand")
+
+        // Audit succès
+        .process(exchange -> {
+            auditHelper.audit(exchange)
+                .ref(
+                    Codifier.FILE_NAME.getCodifier(),
+                    exchange.getProperty("inputFileName", String.class)
+                )
+                .desc("Fichier collateral management traité")
+                .status(Status.Success.getStatus())
+                .data("")
+                .meta("PROCESS_NAME", this.getClass().getName())
+                .send();
+        })
+
+        .log("Collateral management OK: file=${exchangeProperty.inputFileName}");
+    }
+
+    /**
+     * Supprime la dernière ligne du fichier détecté
+     * et dépose le fichier modifié dans le répertoire cible.
+     */
+    private void removeLastLineAndCopy(Exchange exchange) throws Exception {
+        Path inputFile = Paths.get(exchange.getProperty("inputFilePath", String.class));
+        Path outputDir = Paths.get(exchange.getProperty("outputDir", String.class));
+
+        Files.createDirectories(outputDir);
+
+        List<String> lines = Files.readAllLines(inputFile);
+        if (!lines.isEmpty()) {
+            lines.remove(lines.size() - 1);
+        }
+
+        Path target = outputDir.resolve(inputFile.getFileName());
+        Files.write(target, lines);
+
+        exchange.setProperty("outputFile", target.toString());
+    }
+
+    private void handleException(Exchange exchange) throws Exception {
+        Exception ex = exchange.getProperty(Exchange.EXCEPTION_CAUGHT, Exception.class);
+
+        exchange.getMessage().setBody(
+            DollarUUtil.buildNOKDollarURequest(
+                exchange.getProperty("dollarURessourceName", String.class)
+            )
+        );
+        exchange.getContext()
+            .createProducerTemplate()
+            .send("direct:dollarUCommand", exchange);
+
+        log.error(
+            "Collateral management NOK: reason={}",
+            (ex != null ? ex.getMessage() : "unknown"),
+            ex
+        );
+    }
+}
