e9b2fe47ed07611868756d9486e663a13dff39f2
myahi <metnique@gmail.com>
2026-02-07 02:07:19 +0100

    Update start.sh

--- markit/start.sh
+++ markit/start.sh
@@ -2,6 +2,5 @@ nohup "${JAVA_HOME}/bin/java" \
   -Dfile.encoding=UTF-8 \
   -Dlog4j2.configurationFile=file:///serveur_apps/markitDealertEngine/conf/log4j2.xml \
   -jar ../lib/markit_dealer_engine.jar \
-  --spring.config.location=file:///serveur_apps/markitDealertEngine/conf/application.properties \
-  >/serveur_apps/markitDealertEngine/logs/stdout.log 2>/serveur_apps/markitDealertEngine/logs/stderr.log &
-echo $! > /serveur_apps/markitDealertEngine/run/markit.pid
+  --spring.config.location=file:/serveur_apps/markitDealertEngine/conf/application.properties \
+  >> ../log/markit_dealer_engine.log 2>&1 &
