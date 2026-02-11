9811f8cc843b384516c533e7bbec51a304421c19
myahi <metnique@gmail.com>
2026-02-11 14:24:19 +0100

    Delete markit directory

--- markit/start.sh
+++
@@ -1,6 +0,0 @@
-nohup "${JAVA_HOME}/bin/java" \
-  -Dfile.encoding=UTF-8 \
-  -Dlog4j2.configurationFile=file:///serveur_apps/markitDealertEngine/conf/log4j2.xml \
-  -jar ../lib/markit_dealer_engine.jar \
-  --spring.config.location=file:/serveur_apps/markitDealertEngine/conf/application.properties \
-  >/dev/null 2>&1 &
