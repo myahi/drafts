c3f1bec916879b99101e6e90d8c6fb2adcca583a
myahi <metnique@gmail.com>
2026-02-18 14:58:57 +0100

    Update 01.java

--- fix/01.java
+++ fix/01.java
@@ -1,3 +1,37 @@
+private void sendAllocationInstructionAck(Message message,
+                                          SessionID sessionID)
+        throws FieldNotFound, SessionNotFound {
+
+    AllocationInstructionAck ack =
+            new AllocationInstructionAck();
+
+    // === Champs obligatoires ===
+
+    // 70 = AllocID (doit être repris tel quel du AS)
+    ack.set(new AllocID(
+            message.getString(AllocID.FIELD)));
+
+    // 87 = AllocStatus (repris du AS)
+    ack.set(new AllocStatus(
+            message.getInt(AllocStatus.FIELD)));
+
+    // 796 = AllocAckStatus
+    ack.set(new AllocAckStatus(
+            AllocAckStatus.ACCEPTED));
+
+    // 60 = TransactTime (souvent requis par le sell-side)
+    ack.set(new TransactTime(
+            LocalDateTime.now()));
+
+    // === Envoi ===
+    Session.sendToTarget(ack, sessionID);
+
+    LOGGER.info("AllocationInstructionAck sent for AllocID: {}",
+            message.getString(AllocID.FIELD));
+}
+
+
+
 private void sendTradeCaptureReportAck(Message message, SessionID sessionID) throws FieldNotFound, SessionNotFound {
 		TradeCaptureReportAck ack = new TradeCaptureReportAck();
 		ack.set(new TradeReportID(message.getString(TradeReportID.FIELD)));
