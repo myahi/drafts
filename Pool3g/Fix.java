40b68cc4b24ca4b9201f39cc2717195c08d59e6a
myahi <metnique@gmail.com>
2026-02-18 10:19:16 +0100

    Update Fix.java

--- Pool3g/Fix.java
+++ Pool3g/Fix.java
@@ -1,17 +1,27 @@
+@Override
 public Exchange aggregate(Exchange oldEx, Exchange newEx) {
+    // Récupération de la ligne courante (venant du map)
     SfdhMarginCallLine newLine = newEx.getIn().getBody(SfdhMarginCallLine.class);
     List<SfdhMarginCallLine> list;
 
     if (oldEx == null) {
+        // Initialisation de la liste pour le premier passage du groupe (codeBom)
         list = new ArrayList<>();
         list.add(newLine);
+        
+        // On place la liste dans le corps du message
         newEx.getIn().setBody(list);
         return newEx;
     }
 
+    // Récupération de la liste existante pour ce codeBom
     list = oldEx.getIn().getBody(List.class);
-    // Ici, vous pouvez soit ajouter la ligne, 
-    // soit chercher si le codeBom existe déjà pour faire la somme (votre logique métier)
-    list.add(newLine); 
+    
+    // Ajout de la nouvelle ligne à la liste du groupe
+    if (newLine != null) {
+        list.add(newLine);
+    }
+
+    // On retourne l'Exchange contenant la liste mise à jour
     return oldEx;
 }
