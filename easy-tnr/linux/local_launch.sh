6855f180433a791628554f7a84c07b09460519b7
myahi <metnique@gmail.com>
2026-02-21 13:04:31 +0100

    Create local_launch.sh

---
+++ easy-tnr/linux/local_launch.sh
@@ -0,0 +1,7 @@
+/serveur_apps/easy-tnr/scripts/start_batch.sh \
+  DEVEAI \
+  MAESTRO_GLMX_REPO \
+  "MAESTRO/GLMX_TO_CALYPSO/REPO" \
+  -DMaestroDBURL=... \
+  -DMaestroDBUser=... \
+  -DMaestroDBPWD=...
