28d3316d75273694a067437853ee70420188efc3
myahi <metnique@gmail.com>
2026-02-24 13:15:22 +0100

    Delete easy-tnr/V3.java

--- easy-tnr/V3.java
+++
@@ -1,4 +0,0 @@
-fais moi un programme java 8 simple qui charge le contenu d'un repertoire (donne comme parametèreà dans un Set de string en gardant uniquement le contenu entre les balise <ns0:messageContent xmlns:ns0="http://www.lbp.fr/xml"> et </ns0:messageContent>
-puis il charge le contenu d'un autre repertoire dans une map avec comme clé le nom du fichier et comme valeur son contenu
-puis pour chaque element du set chercher dans la map et me donner le nom du fichier trouvé
-
