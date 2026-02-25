4afa7d6fc4632d5f9e11666b8ef68ff9fd60ee6d
myahi <metnique@gmail.com>
2026-02-25 15:37:01 +0100

    Update start_batch.sh

--- easy-tnr/linux/start_batch.sh
+++ easy-tnr/linux/start_batch.sh
@@ -51,10 +51,12 @@ echo " BATCH_NAME   = ${BATCH_NAME}"
 echo " SCENARIOS    = ${SCENARIOS}"
 echo "===================================="
 
-# ✅ Classpath ABSOLU (robuste)
+
 CLASSPATH="${ENV_DIR}/resources:${ENV_DIR}/*"
 
 "${JAVA_EXE}" \
+  -Dfile.encoding=UTF-8 \
+  -Dsun.jnu.encoding=UTF-8 \
   -Duser.home="${ENV_DIR}" \
   "${VM_ARGS[@]}" \
   -Dtnr.configFile="${CONFIG_FILE}" \
