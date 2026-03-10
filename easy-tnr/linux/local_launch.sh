872274cda6b5b5a13239edaedff7279f8007dabd
myahi <metnique@gmail.com>
2026-03-10 09:13:11 +0100

    Delete easy-tnr/linux directory

--- easy-tnr/linux/local_launch.sh
+++
@@ -1,7 +0,0 @@
-/serveur_apps/easy-tnr/scripts/start_batch.sh \
-  DEVEAI \
-  MAESTRO_GLMX_REPO \
-  "MAESTRO/GLMX_TO_CALYPSO/REPO" \
-  -DMaestroDBURL=... \
-  -DMaestroDBUser=... \
-  -DMaestroDBPWD=...
